4742e997ca96ac95c9864c0e8ba379c74c973a11
LydiahWere <lydiahwereanyango@gmail.com>
2023-03-20 09:32:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/**
+/*
  *
- * main - print a line using puts
+ * main - Entry point print a line using puts
  *
  * Return: 0
- */
-int main(void)
+*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
