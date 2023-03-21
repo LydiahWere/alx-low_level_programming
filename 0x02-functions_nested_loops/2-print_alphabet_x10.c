9d5cb7b64ffad6e9f683d2c534bc88af9f2e9247
LydiahWere <lydiahwereanyango@gmail.com>
2023-03-21 12:34:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print _alphabet_x10 - print the alphabet 10times
- *
- * Return: always 0 (success)
+ * print _alphabet_x10 - print the alphabet 10times in
+ *lowercase followed by new line
  */
-void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char alpha;
