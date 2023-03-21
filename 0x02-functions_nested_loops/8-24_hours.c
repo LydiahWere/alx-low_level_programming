510bbcefb23e47dc06662113717317e92a013973
LydiahWere <lydiahwereanyango@gmail.com>
2023-03-21 15:55:46 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,10 +8,11 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0
+	i = 0;
 		while (i < 24)
-		{j = 0;
-			while (j < 60)
+		{
+			j = 0;
+				while (j < 60)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
@@ -19,8 +20,8 @@ void jack_bauer(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 				_putchar('\n');
-				j++
+				j++;
 			}
-			i++
+			i++;
 		}
 }
