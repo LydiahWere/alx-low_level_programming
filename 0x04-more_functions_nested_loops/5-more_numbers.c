2a0b7142df3e7decee1089fc9b8de5718c4cc909
LydiahWere <lydiahwereanyango@gmail.com>
2023-03-23 12:51:16 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * more_numbers - nmbr fct
- * Return: no return
+ * Return
  */
 
 void more_numbers(void)
@@ -13,9 +13,10 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar('1')
-				_putchar(j % 10 + '0');
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-				_putchar('\n');
+		_putchar('\n');
 	}
 }
+
